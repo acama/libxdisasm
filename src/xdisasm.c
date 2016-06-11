a985f4fe56c96d83288737a693f5dac7f7a0efe6
Amat Cama <amatcama@gmail.com>
2016-06-10 18:27:42 -0700

    bfd support for sparc elf files

--- src/xdisasm.c
+++ src/xdisasm.c
@@ -280,7 +280,7 @@ int init_dis_env(int arch, int bits, int endian){
             disas = print_insn_riscv;
             break;
 
-        case ARCH_sparc:
+        case ARCH_sparc: // TODO: use the different mach's based on the mach from the ELF
             if (bits == 64) dis->mach = bfd_mach_sparc_v9; // v9 implements sparc64
             disas = print_insn_sparc;
             break;
